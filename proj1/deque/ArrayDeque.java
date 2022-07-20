2322d8038f42910ddf25f277256a4f2d0e48d080
A.R.L <luminyouyazj@126.com>
2022-07-20 20:54:16 +0800

    debug resize down(remove)

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -66,17 +66,17 @@ public class ArrayDeque<T> implements Deque<T>{
         if(fsize <= 0){
             res = last[-fsize];
             last[-fsize] = null;
+            if(lsize > 2 && lsize < last.length/4){
+                last = resize(lsize/2,last,lsize);
+            }
         }else {
             res = first[fsize-1];
             first[fsize-1] = null;
+            if(fsize > 2 && fsize < first.length/4){
+                first = resize(fsize/2,first,fsize);
+            }
         }
         fsize -= 1;
-        if(fsize < first.length/4){
-            first = resize(fsize/2,first,fsize);
-        }
-        if(lsize < last.length/4){
-            last = resize(lsize/2,last,lsize);
-        }
         return res;
     }
 
@@ -89,17 +89,17 @@ public class ArrayDeque<T> implements Deque<T>{
         if(lsize <= 0){
             res = first[-lsize];
             first[-lsize] = null;
+            if(fsize > 2 && fsize < first.length/4){
+                first = resize(fsize/2,first,fsize);
+            }
         }else {
             res = last[lsize-1];
             last[lsize-1] = null;
+            if(lsize > 2 && lsize < last.length/4){
+                last = resize(lsize/2,last,lsize);
+            }
         }
         lsize -= 1;
-        if(fsize < first.length/4){
-            first = resize(fsize/2,first,fsize);
-        }
-        if(lsize < last.length/4){
-            last = resize(lsize/2,last,lsize);
-        }
         return res;
     }
 
